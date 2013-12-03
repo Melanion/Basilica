31cd5b1a6b22244ae64b3155b4ac0918ac326eee
Wes Johnson <megakeen@gmail.com>
2013-12-02 22:55:22 -0500

    Basic App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Basilica::Application.config.secret_token = '6a12463217d174ee769efaef5fa88a8bb744783b0bf295cd5236da8d1083617afa7247ed35a7778bf236df818bf57f4234f851f46ba94cac4e5bae0a27f7466b'
