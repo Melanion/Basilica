2615c1165c1c65d7fbe5e5360953af4ba872fa45
Wes Johnson <megakeen@gmail.com>
2014-03-07 13:45:49 -0500

    Dummied-out step file

---
+++ features/step_definitions/navigation_steps.rb
@@ -0,0 +1,119 @@
+Given(/^I am not signed in$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I visit the main page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I click an item's "(.*?)" button$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be redirected to the sign\-in screen$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am signed in as a user$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the base quantity of that item should be added to my cart$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I visit the shopping cart page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see a list of items in my cart$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see a quantity next to each item$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see an "(.*?)" button$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see a "(.*?)" button$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am at the shopping cart page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I type a new value into a quantity field$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I hit submit$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the displayed quantity of the item should be changed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I click the "(.*?)" button$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the list of items in my cart should be empty$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I have sufficient credits to exchange for all my items$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the items in my cart should transfer to my personal inventory$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^my cart should be empty$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^my credits should decrease by the total price of the items$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I do not have sufficient credits to exchange for all my items$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see an "(.*?)" warning$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^my cart should not change$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see a list of items$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)" buttons next to each item$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am signed in as an administrator$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I visit my inventory page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see a list of the items in my inventory$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see my total credits$/) do
+  pending # express the regexp above with the code you wish you had
+end
